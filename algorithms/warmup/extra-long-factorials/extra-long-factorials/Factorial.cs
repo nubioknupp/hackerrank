902d584dce577a1549f89bf9809e9d1d6d07b1ee
Núbio Knupp <nubioknupp@gmail.com>
2015-08-22 21:20:52 -0300

    Resolution Extra long factorials
    Resolution Extra long factorials

---
+++ algorithms/warmup/extra-long-factorials/extra-long-factorials/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace extra_long_factorials
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            BigInteger x = 1;
+
+            for (var i = 1; i <= n; i++)
+            {
+                x = x * i;
+            }
+
+            Console.WriteLine(x);
+        }
+
+    }
+}
