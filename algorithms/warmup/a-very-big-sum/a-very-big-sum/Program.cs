bf5aa715f932667d83a2bf67c8d00d8d45f4fd7f
Núbio Knupp <nubioknupp@gmail.com>
2015-09-08 21:45:21 -0300

    Resolution A Very Big Sum.
    Resolution A Very Big Sum.

---
+++ algorithms/warmup/a-very-big-sum/a-very-big-sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace a_very_big_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Convert.ToInt32(Console.ReadLine());
+            var numbers = (Console.ReadLine() + "").Split();
+            decimal sun = 0;
+
+
+            for (int i = 0; i < size; i++)
+            {
+                sun += decimal.Parse(numbers[i]);
+            }
+
+            Console.WriteLine(sun);
+        }
+    }
+}
