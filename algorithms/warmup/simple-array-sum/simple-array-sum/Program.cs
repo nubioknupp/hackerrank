a7567bab90f4c370913723da9628c25975d818fb
Núbio Knupp <nubioknupp@gmail.com>
2015-09-08 21:04:17 -0300

    Resolution Simple Array Sum
    Resolution Simple Array Sum

---
+++ algorithms/warmup/simple-array-sum/simple-array-sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace simple_array_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Convert.ToInt32(Console.ReadLine());
+            var numbers = (Console.ReadLine() + "").Split();
+            var sun = 0;
+
+
+            for (int i = 0; i < size; i++)
+            {
+                sun += Convert.ToInt32(numbers[i]);
+            }
+
+            Console.WriteLine(sun);
+        }
+    }
+}
