327d4e481dfad68da115e1166fcb13ca4986bb23
Núbio Knupp <nubioknupp@gmail.com>
2015-09-10 22:18:35 -0300

    Resolution Diagonal Difference.
    Resolution Diagonal Difference.

---
+++ algorithms/warmup/diagonal-difference/diagonal-difference/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace diagonal_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Convert.ToInt32(Console.ReadLine());
+            var matrix = new int[size, size];
+            var diagA = 0;
+            var diagB = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                var temp = (Console.ReadLine() + "").Split();
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(temp[j]);
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                diagA += matrix[i, i];
+                diagB += matrix[i, (size - 1) - i];
+            }
+
+            Console.WriteLine(Math.Abs(diagA - diagB));
+
+        }
+    }
+}
