80d7ac21ebbfb117fc43aa30955755e6d025876c
Núbio Knupp <nubioknupp@gmail.com>
2015-09-11 23:08:43 -0300

    Resolution Time Conversion.
    Resolution Time Conversion.

---
+++ algorithms/warmup/time-conversion/time-conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace time_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine(time.ToString("HH:mm:ss"));
+        }
+    }
+}
