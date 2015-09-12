67fa39d085b1baa2d3f85385e74f3df669d003d7
Núbio Knupp <nubioknupp@gmail.com>
2015-09-11 22:39:49 -0300

    Resolution Staircase.
    Resolution Staircase.

---
+++ algorithms/warmup/Staircase/Staircase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Convert.ToInt32(Console.ReadLine());
+            var value = "";
+
+            for (int i = 0; i < size; i++)
+            {
+                value += "#";
+                Console.WriteLine(value.PadLeft(size));
+            }
+        }
+    }
+}
